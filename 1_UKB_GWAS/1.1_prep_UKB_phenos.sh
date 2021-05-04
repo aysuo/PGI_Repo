104e4622a5022e8d9e9210b3cd4e60aee0eddf53
aysuo <aysuokbay@gmail.com>
2021-05-04 14:10:34 -0400

    Cleaned step 2, scrapped paths, now reading all paths from file.

--- 1_UKB_GWAS/1.1_prep_UKB_phenos.sh
+++ 1_UKB_GWAS/1.1_prep_UKB_phenos.sh
@@ -1,21 +1,19 @@
 
 #!/bin/bash
 
-cd /disk/genetics4/PGS/Aysu/PGS_Repo_pipeline/derived_data/1_UKB_GWAS
+source paths1
 
-dirCode="/disk/genetics4/PGS/Aysu/PGS_Repo_pipeline/code/1_UKB_GWAS"
-brain_file="/disk/genetics/PGS/PGS_Repo/data/UKB_pheno/k11425.f12188.R.tab"
-relatedness_file="/disk/genetics/PGS/PGS_Repo/data/UKB_pheno/ukb11425_rel_s488282.dat"
+cd $p1_UKBGWAS
 
-###### GET LIST OF BRAIN MRI SAMPLE AND RELATEDS FOR PARTITIONING ########
+###### GET LIST OF BRAIN MRI SAMPLE AND RELATED INDIVIDUALS FOR PARTITIONING ########
 echo "Getting list of brain-imaged and up to 3rd degree related individuals.." 
 
-### UKB brain imaging sample (file "k11425.f12188.R.tab" prepared by Richard based on data field 12188)
-awk -F"\t" 'NR>1 && !($2==0 && $3==0){print $1,"1"}' OFS="\t" $brain_file > tmp/brain_sample
+### UKB brain imaging sample (file "k11425.f12188.R.tab" based on data field 12188)
+awk -F"\t" 'NR>1 && !($2==0 && $3==0){print $1,"1"}' OFS="\t" $p1_UKBbrain > tmp/brain_sample
 
-### UKB relatedness data (also shared by Richard) - Contains relatives up to third degree
+### UKB relatedness data - Contains relatives up to third degree
 # Remove withdrawn individuals (negative ID), collapse, reverse sort on ID and kinship  
-awk 'NR>1 && $1>0 && $2>0{print $1,$5;print $2,$5}' OFS="\t" $relatedness_file | sort -k1,5 -r > tmp/tmp
+awk 'NR>1 && $1>0 && $2>0{print $1,$5;print $2,$5}' OFS="\t" $p1_UKBrelatedness | sort -k1,5 -r > tmp/tmp
 
 # Remove duplicate ID's, keeping the one with larger kinship, then reverse sort on kinship
 awk -F"\t" '!_[$1]++' OFS="\t" tmp/tmp | sort -k2 -g -r > tmp/UKBv2_rel_uniq_sorted
@@ -28,7 +26,15 @@ echo "List created."
 
 echo "Running Stata to partition sample and obtain phenotype file for each partition.."
 
-stata -b do $dirCode/1.1_prep_UKB_phenos.do
+stata -b do $p1_code/1.1_prep_UKB_phenos.do \
+	$p1_UKBGWAS \
+	$p1_UKBcrosswalk \
+	$p1_UKB_partition_data \
+	$p1_pheno_data_1 \
+	$p1_pheno_data_2 \
+	$p1_pheno_data_3 \
+	$p1_covar_data \
+	$p1_log
 
 # Replace missing values (.) with NA
 for file in input/*.pheno; do
--- 1_UKB_GWAS/1.2_BOLTLMM_master.sh
+++ 1_UKB_GWAS/1.2_BOLTLMM_master.sh
@@ -1,66 +1,24 @@
 #!/bin/bash
 
-########################################################
-####### SCRIPT FOR RUNNING GWAS WITH BOLT LMM ##########
-########################################################
+source paths1
 
-#### Requirements
-  ## BOLT LMM software (http://data.broadinstitute.org/alkesgroup/BOLT-LMM/downloads/)
-  ## Phenotype data in the form of [FID IID resid]
-    # ID format should be the same across phenotype and genotype data
-    # Missing phenotype should have -9 in resid column
-  ## Genotype data in plink format that contain model SNPs (~500K) to be used in estimating GRM
-  ## Imputed genotype data in the format of your choice to be used for association analysis
+cd $p1_UKBGWAS
+mkdir -p output
+mkdir -p logs
 
-#### Documentation
-  ## For detailed documentation on running bolt (especially with UKB data), refer to wiki (https://github.com/omeed-maghzian/ssgac/wiki/GWAS-in-UKB2-with-BOLT-LMM)
+## pheno column name
+pheno_col="resid"
 
+## QC filters and bolt options
+bgen_MAF=0.01 # MAF filter for bgen genotype data
+bgen_INFO=0.7 # INFO filter for bgen genotype data
+num_Threads=7 # number of cores to use
 
 
-
-########################################################
-###################### USER INPUT ######################
-########################################################
-
-#### Fill in the quotes below.
-
-    ## Working directory
-    WD="/disk/genetics/PGS/Aysu/PGS_Repo_pipeline/derived_data/1_UKB_GWAS"
-    cd $WD
-    mkdir -p output
-    mkdir -p logs
-
-    ## path to bolt-lmm package
-    bolt_path="/disk/genetics2/pub/software/bolt/BOLT-LMM_v2.3"
-
-    ## path to genotype data in plink format that contain model SNPs
-    genotype_plink_fam="/disk/genetics2/ukb/orig/UKBv2/bolt_modelsnps/ukb2_maf01_info60_rsq30_nodup_recoded_pruned_chr1.fam"
-    genotype_plink_bed="/disk/genetics2/ukb/orig/UKBv2/bolt_modelsnps/ukb2_maf01_info60_rsq30_nodup_recoded_pruned_chr{1:22}.bed"
-    genotype_plink_bim="/disk/genetics2/ukb/orig/UKBv2/bolt_modelsnps/ukb2_maf01_info60_rsq30_nodup_recoded_pruned_chr{1:22}.bim"
-
-    ## path to imputed genotype data in the bgen/sample format (BGEN 1.2 format)
-    genotype_bgen="/disk/genetics2/ukb/orig/UKBv2/decrypted/ukb_imp_chr{1:22}_v2.bgen"
-    genotype_sample="/disk/genetics2/ukb/orig/UKBv2/linking/ukb_imp_v2.sample"
-
-    ## path to phenotype data
-    pheno_col="resid"
-
-    ## QC filters and bolt options
-    bgen_MAF=0.01 # MAF filter for bgen genotype data
-    bgen_INFO=0.7 # INFO filter for bgen genotype data
-    num_Threads=7 # number of cores to use
-
-
-########################################################
-################## END OF USER INPUT ###################
-########################################################
-
-
-# 1: pheno
 run_BOLTLMM(){
   pheno=$1
-  mkdir -p ${WD}/output/$pheno
-  mkdir -p ${WD}/logs/$pheno
+  mkdir -p ${p1_UKBGWAS}/output/$pheno
+  mkdir -p ${p1_UKBGWAS}/logs/$pheno
   
   echo "----------------------------------------------------------------------"
 
@@ -72,25 +30,25 @@ run_BOLTLMM(){
   start=$(date +%s)
 
   
-  for j in `seq 1 3` # for partitions 1 through 3
+  for j in 1 2 3 # for partitions 1 through 3
   do
     #### set file names
     pheno_file="input/UKB_${pheno}_part${j}.pheno"
 
-    nohup ${bolt_path}/bolt --lmm \
-      --LDscoresFile=${bolt_path}/tables/LDSCORE.1000G_EUR.tab.gz \
-      --fam=${genotype_plink_fam} \
-      --bed=${genotype_plink_bed} \
-      --bim=${genotype_plink_bim} \
-      --bgenFile=${genotype_bgen} \
-      --sampleFile=${genotype_sample} \
+    nohup ${p1_bolt}/bolt --lmm \
+      --LDscoresFile=${p1_bolt}/tables/LDSCORE.1000G_EUR.tab.gz \
+      --fam=${p1_gf_fam} \
+      --bed=${p1_gf_bed} \
+      --bim=${p1_gf_bim} \
+      --bgenFile=${p1_gf_bgen} \
+      --sampleFile=${p1_gf_sample} \
       --numThreads=${num_Threads} \
       --bgenMinMAF=${bgen_MAF} \
       --bgenMinINFO=${bgen_INFO} \
       --phenoFile=${pheno_file} \
       --phenoCol=${pheno_col} \
-      --statsFile=${WD}/output/${pheno}/UKB_${pheno}_part${j}_BOLTLMM_plink \
-      --statsFileBgenSnps=${WD}/output/${pheno}/UKB_${pheno}_part${j}_BOLTLMM > ${WD}/logs/${pheno}/UKB_${pheno}_part${j}_BOLTLMM.log &
+      --statsFile=${p1_UKBGWAS}/output/${pheno}/UKB_${pheno}_part${j}_BOLTLMM_plink \
+      --statsFileBgenSnps=${p1_UKBGWAS}/output/${pheno}/UKB_${pheno}_part${j}_BOLTLMM > ${p1_UKBGWAS}/logs/${pheno}/UKB_${pheno}_part${j}_BOLTLMM.log &
   done
   wait
 
@@ -108,7 +66,7 @@ run_BOLTLMM(){
 
 main(){
   # Get list of phenotypes
-  pheno_list=$(ls $WD/input/*.pheno | rev | cut -d"/" -f1 | rev | sed 's/UKB_//g' | sed 's/_part[1-3]\.pheno//g' | sort | uniq)
+  pheno_list=$(ls $p1_UKBGWAS/input/*.pheno | rev | cut -d"/" -f1 | rev | sed 's/UKB_//g' | sed 's/_part[1-3]\.pheno//g' | sort | uniq)
 
   for pheno in $pheno_list; do
     run_BOLTLMM $pheno
