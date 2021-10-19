a442a909aedf47f69b95bac94b364063686648f7
aysuo <aysuokbay@gmail.com>
2021-10-19 07:58:16 -0400

    Added effective N filter, now dropping SNPs with Neff<0.8*median(Neff) from MTAG output

--- 2_Formatting/2.5.1_Format_MTAG.sh
+++ 2_Formatting/2.5.1_Format_MTAG.sh
@@ -45,6 +45,10 @@ format_MTAG_LDpred(){
 }
 
 
+
+
+    
+# TO DO: Make sure LDSC accepts the SBayesR column names (may need to alter script 5.0 to specify col names with flags)
 format_MTAG_SBayesR(){
     dirIn=$1
     multi=$2
@@ -61,21 +65,33 @@ format_MTAG_SBayesR(){
         sumstats=$(ls $dirIn/*trait_1.txt)
     fi
     
-    # Formatted output will be named XXX_formatted.txt
+    # Formatted output will be named XXX_formatted_SBayesR.txt
     out=$(echo $sumstats | sed 's/\.txt/_formatted_SBayesR\.txt/g')
     declare -a sumstats=$(echo "($sumstats)")
     declare -a out=$(echo "($out)")
 
     numSumstats=${#sumstats[@]}
+    Nsnps=$(($(wc -l ${sumstats[0]} | cut -d" " -f1)-1))
+    medianSNP=$((($Nsnps/2)+1))
 
     for (( i=0; i<$numSumstats; i++ )); do
         # Get GWAS-equivalent N from MTAG log file
-        gwasN=$(grep -A $(($i+1)) "GWAS equiv. (max) N" $pheno/$pheno.log | tail -1  | awk '{print $NF}')
+        gwasN=$(grep -A $(($i+1)) "GWAS equiv. (max) N" $dirIn/$pheno.log | tail -1  | awk '{print $NF}')
+        
+        # Get median effective N 
+        awk -F"\t" 'NR==1 { for (i=1; i<=NF; i++) { ix[$i] = i } } \
+            NR>1 {print 1/(2*$ix["mtag_se"]*$ix["mtag_se"]*$ix["FRQ"]*(1-$ix["FRQ"]))}' OFS="\t" ${sumstats[$i]} | sort -g > $dirIn/effN_$i
+        medianN=$(awk -v M=${medianSNP} 'NR==M{print}' $dirIn/effN_$i )
+        maxN=$(tail -1 $dirIn/effN_$i | cut -d" " -f2)
+        minN=$(head -1 $dirIn/effN_$i | cut -d" " -f2)
+        
         # Format file, adding in total N and GWAS-eq N
-        awk -F"\t" -v gwasN=$gwasN 'NR==1 { for (i=1; i<=NF; i++) { ix[$i] = i }; print "SNP","A1","A2","freq","b","se","p","N" }\
-            NR>1 {print $ix["SNP"],$ix["A1"],$ix["A2"],$ix["FRQ"],$ix["mtag_beta"],$ix["mtag_se"],$ix["mtag_pval"],gwasN}' ${sumstats[$i]} > ${out[$i]} &
-    done 
-    wait
-    
+        awk -F"\t" -v gwasN=$gwasN -v medianN=$medianN 'NR==1 { for (i=1; i<=NF; i++) { ix[$i] = i }; print "SNP","A1","A2","freq","b","se","p","N" }\
+            NR>1 {effN=1/(2*$ix["mtag_se"]*$ix["mtag_se"]*$ix["FRQ"]*(1-$ix["FRQ"]))}\
+            NR>1 && effN>0.8*medianN {print $ix["SNP"],$ix["A1"],$ix["A2"],$ix["FRQ"],$ix["mtag_beta"],$ix["mtag_se"],$ix["mtag_pval"],gwasN}' ${sumstats[$i]} > ${out[$i]} 
+        NsnpsOut=$(wc -l ${out[$i]} | cut -d" " -f1)
+     
+        echo "$pheno - $i: median effN = $medianN, min effN = $minN, max effN = $maxN, GWAS-eq N = $gwasN, $(($Nsnps-$NsnpsOut)) SNPs dropped." >> $dirIn/../SBayesR_Nfiltering.txt
+    done
     echo "Formatting for $pheno finished."
 }
