b4c347cac88b7109ffc344866a5d2c63a260b02b
aysuo <aysuokbay@gmail.com>
2021-12-02 08:00:10 -0500

    Apparently changed loop index var from i to j (don't remember why?)

--- 2_Formatting/2.5_Format_MTAG_single_wrapper.sh
+++ 2_Formatting/2.5_Format_MTAG_single_wrapper.sh
@@ -4,14 +4,14 @@ source $PGI_Repo/code/2_Formatting/2.5.1_Format_MTAG.sh
 
 cd $PGI_Repo/derived_data/4_MTAG_single
 
-i=0
+j=0
 for pheno in $PGI_Repo/derived_data/4_MTAG_single/*; do
     format_MTAG_SBayesR $pheno 0 &
-    let i+=1
+    let j+=1
 
     # Run 10 at a time
-    if [[ $i == 10 ]]; then
+    if [[ $j == 10 ]]; then
         wait
-        i=0
+        j=0
     fi   
 done
