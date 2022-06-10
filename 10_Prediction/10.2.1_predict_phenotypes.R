de2de5065a23e85f2acb4cd1009abb85e61c3646
aysuo <aysuokbay@gmail.com>
2022-06-10 09:29:49 -0400

    Add CI's for SBayesR-LDpred diff

--- 10_Prediction/10.2.1_predict_phenotypes.R
+++ 10_Prediction/10.2.1_predict_phenotypes.R
@@ -23,7 +23,11 @@ lapply(packages, library, character.only = TRUE)
 # Parse arguments
 args=commandArgs(trailingOnly=TRUE)
 cohort=args[1]
-method=args[2]
+
+# Score types: Public, single-trait, multi-trait
+#score_types=c("public","single","multi")
+score_types=c("single_SBayesR","single_LDpred")
+
 
 ########################################################
 ###################### Data paths ######################
@@ -38,40 +42,24 @@ pheno_files <- list.files(pheno_wd)
 # Phenotype names
 pheno_names <- gsub(".pheno", "", pheno_files)
 
-# Score types: Public, single-trait, multi-trait
-score_types=c("public","single","multi")
-#score_types=c("single")
+predict <- c()
 
-# Loop over score types
-for(i in score_types){
-    # Set directory containing scores for the score type
+# Loop over PGI types
+for ( i in score_types ){
+    # PGI directory
     assign(paste0(i,"_score_wd"), paste0("derived_data/9_Scores/",i,"/scores/"))
-    assign(paste0(i,"_score_files"), list.files(eval(parse(text=paste0(i,"_score_wd")))))
+    assign(paste0(i,"_score_files"), list.files(eval(parse(text=paste0(i,"_score_wd"))), cohort))
     
-    # Get list of all score files for the score type
-    score_files <- eval(parse(text=paste0(i,"_score_files")))
-
-    # Get list of score files for input cohort
-    #assign(paste0(cohort,"_",i,"_score_files"),score_files[grep(cohort, score_files)])
-    assign(paste0(cohort,"_",i,"_score_files"),grep(method, grep(cohort, score_files, value=T),value=T))
-
-    # Score names for input cohort
-    #assign(paste0(i,"_score_names"), gsub(paste0("PGS_",cohort,"_"), "", gsub("_LDpred_p1.txt", "", 
-    #    gsub("-.*", "",eval(parse(text=paste0(cohort,"_",i,"_score_files")))))))
-    if (method == "LDpred") {
-        methodSuffix = "_LDpred_p1.txt"
-    } else {
-        methodSuffix = "SBayesR.txt"
-    }    
-    assign(paste0(i,"_score_names"), gsub(paste0("PGS_",cohort,"_"), "", gsub(methodSuffix, "", gsub("-.*", "",eval(parse(text=paste0(cohort,"_",i,"_score_files")))))))
-
-    # Overlapping names (both pheno and score available)
+    # PGI names
+    assign(paste0(i,"_score_names"), gsub(paste0("PGS_",cohort,"_"), "", gsub("-.*", "",eval(parse(text=paste0(i,"_score_files"))))))
+  
+    # Overlapping names (both pheno and PGI available)
     assign(paste0("predict_",i), pheno_names[pheno_names %in% eval(parse(text=paste0(i,"_score_names")))])
+    
+    predict <- union(predict, eval(parse(text=paste0("predict_",i))))
 }
+pheno_names <- predict
 
-# List of phenotypes for which there's at least one score
-pheno_names <- union(predict_single, predict_multi)
-#pheno_names <- predict_single
 
 # If cohort is HRS, need pheno-geno crosswalk
 if ( cohort == "HRS2" ){
@@ -96,16 +84,15 @@ if ( cohort == "UKB3" ){
     rename_at(vars(PCs_oldnames), ~ PCs_newnames)
 }
 
-
-
-### empty data frame with r^2
+### Empty data frame with r^2
 df <- data.frame(phenotype = character())
 bootstrap_df <- data.frame(
   iterations = numeric(),
   phenotype = character(),
-  inc_r2_public = numeric(),
-  inc_r2_single = numeric(),
-  inc_r2_multi = numeric()
+  inc_r2_public_LDpred = numeric(),
+  inc_r2_single_LDpred = numeric(),
+  inc_r2_multi_LDpred = numeric(),
+  inc_r2_single_SBayesR = numeric()
 )
 #####################################################3
 
@@ -114,7 +101,7 @@ iterations <- 1000
 
 # Loop over phenotypes
 for (i in 1:length(pheno_names)){
-
+    
     local_df <- data.frame(phenotype = character(1))
     pheno <- pheno_names[i]
     local_df$phenotype <- pheno
@@ -122,12 +109,12 @@ for (i in 1:length(pheno_names)){
     # Read in pheno data
     pheno_path <- paste0(pheno_wd,"/", pheno, ".pheno")
     pheno_data <- fread(pheno_path)
-
+    
     score_data <- NULL
-    # Loop over score types (single, multi, public) available for the phenotype
+    # Loop over score types
     for (i in score_types) {
-        score_files <- eval(parse(text=paste0(cohort,"_",i,"_score_files")))   
-
+        score_files <- eval(parse(text=paste0(i,"_score_files")))   
+        
         # Score file for pheno
         score_file <- score_files[grep(paste0("_",pheno,"-"), score_files, fixed=TRUE)]
 
@@ -152,7 +139,7 @@ for (i in 1:length(pheno_names)){
             assign(paste0(pheno,"_",i),"FALSE")
         }
     }  
-
+    
     # Merge with pheno and PC data
     # HRS
     if ( cohort == "HRS2" ){
@@ -196,43 +183,42 @@ for (i in 1:length(pheno_names)){
         BATCH <- str_c(paste0("batch",1:106),collapse=" + ")
         PCs <- paste(PCs,BATCH,sep=" + ")
     }
-    formula_no_score <- as.formula(paste0("phenotype ~ ", PCs))
-    formula_with_public_score <- as.formula(paste0("phenotype ~ score_public + ", PCs))
-    formula_with_single_score <- as.formula(paste0("phenotype ~ score_single + ", PCs))
-    formula_with_multi_score <- as.formula(paste0("phenotype ~ score_multi + ", PCs))
     
+    formula_no_score <- as.formula(paste0("phenotype ~ ", PCs))
     reg_no_score <- lm(formula_no_score, data)
     local_df$r2_no_score <- summary(reg_no_score)$r.squared * 100
 
+    local_df$r2_inc_single_LDpred <- NA
+    local_df$r2_inc_single_SBayesR <- NA
+    #local_df$r2_inc_multi_LDpred <- NA
+    #local_df$r2_inc_public_LDpred <- NA
+    
+    
     for (k in score_types){
+        assign(paste0("formula_with_",k,"_score"),as.formula(paste0("phenotype ~ score_", k, " + ", PCs)))
         reg <- eval(parse(text=paste0(pheno,"_",k)))
             if ( reg == TRUE ){
-                assign(paste0("reg_with_",k,"_score"), lm(eval(parse(text=paste0("formula_with_",k,"_score"))), data))
-                assign(paste0("r2_with_",k,"_score"), eval(parse(text=paste0("summary(reg_with_",k,"_score)$r.squared"))) * 100)
+                reg_with_score <- lm(eval(parse(text=paste0("formula_with_",k,"_score"))), data)
+                r2 <-summary(reg_with_score)$r.squared*100
             } else {
-                assign(paste0("r2_with_",k,"_score"),NA)
+                r2 <- NA
             }
+        local_df[1,paste0("r2_inc_",k)] <- r2 - local_df$r2_no_score[1]
     }
 
-    local_df$r2_with_public_score <- r2_with_public_score
-    local_df$r2_with_single_score <- r2_with_single_score
-    local_df$r2_with_multi_score <- r2_with_multi_score
-
-    local_df$r2_inc_public <- local_df$r2_with_public_score - local_df$r2_no_score
-    local_df$r2_inc_single <- local_df$r2_with_single_score - local_df$r2_no_score
-    local_df$r2_inc_multi  <- local_df$r2_with_multi_score - local_df$r2_no_score
-
-    local_df$r2_diff_single_public  <- local_df$r2_with_single_score - local_df$r2_with_public_score
-    local_df$r2_diff_multi_public  <- local_df$r2_with_multi_score - local_df$r2_with_public_score
-    local_df$r2_diff_multi_single  <- local_df$r2_with_multi_score - local_df$r2_with_single_score
-
+    #local_df$r2_diff_single_LDpred_public_LDpred  <- local_df$r2_inc_single_LDpred - local_df$r2_inc_public_LDpred
+    #local_df$r2_diff_multi_LDpred_public_LDpred  <- local_df$r2_inc_multi_LDpred - local_df$r2_inc_public_LDpred
+    #local_df$r2_diff_multi_LDpred_single_LDpred  <- local_df$r2_inc_multi_LDpred - local_df$r2_inc_single_LDpred
+    local_df$r2_diff_single_SBayesR_single_LDpred  <- local_df$r2_inc_single_SBayesR - local_df$r2_inc_single_LDpred
+    
     # Vectors for bootstrapping data
-    r2_inc_public_list <- c()
-    r2_inc_single_list <- c()
-    r2_inc_multi_list <- c()
-    r2_diff_single_public_list <- c()
-    r2_diff_multi_public_list <- c()
-    r2_diff_multi_single_list <- c()
+    for (k in score_types){
+        assign(paste0("r2_inc_",k,"_list"), c())
+    }
+    #r2_diff_single_LDpred_public_LDpred_list <- c()
+    #r2_diff_multi_LDpred_public_LDpred_list <- c()
+    #r2_diff_multi_LDpred_single_LDpred_list <- c()
+    r2_diff_single_SBayesR_single_LDpred_list <-c()
 
     # Start bootstrapping R2's
     for (j in 1:iterations) {
@@ -245,94 +231,85 @@ for (i in 1:length(pheno_names)){
 
         # resample, regress, add r2
         resampled_data <- sample_n(data, nrow(data), replace=TRUE)
-        resampled_no_score_reg_fit <- lm(formula_no_score, resampled_data)
-        resampled_no_score_reg <- summary(resampled_no_score_reg_fit)
-        r2_no_score <- resampled_no_score_reg$r.squared * 100
+        resampled_no_score_reg <- lm(formula_no_score, resampled_data)
+        resampled_r2_no_score <- summary(resampled_no_score_reg)$r.squared * 100
         
         for (k in score_types){
             reg=eval(parse(text=paste0(pheno,"_",k)))
             if ( reg == TRUE ){
-                assign(paste0("resampled_with_",k,"_score_reg_fit"), lm(eval(parse(text=paste0("formula_with_",k,"_score"))), resampled_data))
-                assign(paste0("r2_with_",k,"_score"), eval(parse(text=paste0("summary(resampled_with_",k,"_score_reg_fit)$r.squared"))) * 100)
+                resampled_reg_with_score <- lm(eval(parse(text=paste0("formula_with_",k,"_score"))), resampled_data)
+                resampled_r2 <-summary(resampled_reg_with_score)$r.squared*100
             } else {
-                assign(paste0("r2_with_",k,"_score"), NA)
+                resampled_r2 <- NA
             }
-        }
 
-        r2_inc_public_list[j] <- r2_with_public_score - r2_no_score
-        r2_inc_single_list[j] <- r2_with_single_score - r2_no_score
-        r2_inc_multi_list[j] <- r2_with_multi_score - r2_no_score
-
-        r2_diff_single_public_list[j] <- r2_with_single_score - r2_with_public_score
-        r2_diff_multi_public_list[j] <- r2_with_multi_score - r2_with_public_score
-        r2_diff_multi_single_list[j] <- r2_with_multi_score - r2_with_single_score
+            assign(eval(paste0("r2_inc_",k,"_list")), c(eval(parse(text=paste0("r2_inc_",k,"_list"))), resampled_r2-resampled_r2_no_score))
+        }    
         
     }
+    r2_diff_single_SBayesR_single_LDpred_list <- r2_inc_single_SBayesR_list - r2_inc_single_LDpred_list
+    #r2_diff_single_LDpred_public_LDpred_list <- r2_inc_single_LDpred_list - r2_inc_public_LDpred_list
+    #r2_diff_multi_LDpred_public_LDpred_list <- r2_inc_multi_LDpred_list - r2_inc_public_LDpred_list
+    #r2_diff_multi_LDpred_single_LDpred_list <- r2_inc_multi_LDpred_list - r2_inc_single_LDpred_list 
 
     # save bootstrapped r2s
     local_bootstrap_df <- data.frame(
-        iteration = 1:1000,
+        iteration = 1:iterations,
         phenotype = pheno
     )
-    
-    local_bootstrap_df$inc_r2_public <- r2_inc_public_list
-    local_bootstrap_df$inc_r2_single <- r2_inc_single_list
-    local_bootstrap_df$inc_r2_multi <- r2_inc_multi_list
-
     bootstrap_df <- rbind(bootstrap_df, local_bootstrap_df)
 
-    # generate quantities of interest
-    r2_inc_public_low  <- quantile(r2_inc_public_list, .025, na.rm=T)[[1]]
-    r2_inc_public_high <- quantile(r2_inc_public_list, .975, na.rm=T)[[1]]
-    r2_inc_public_mean <- mean(r2_inc_public_list)
+    for (k in score_types){
+        assign(eval(paste0("local_bootstrap_df$inc_r2_",k)), eval(parse(text=paste0("r2_inc_",k,"_list"))))
+        assign(paste0("r2_inc_",k,"_low"), quantile(eval(parse(text=paste0("r2_inc_",k,"_list"))),.025, na.rm=T)[[1]])
+        assign(paste0("r2_inc_",k,"_high"), quantile(eval(parse(text=paste0("r2_inc_",k,"_list"))),.975, na.rm=T)[[1]])
+        assign(paste0("r2_inc_",k,"_mean"), mean(eval(parse(text=paste0("r2_inc_",k,"_list")))))
+    }
 
-    r2_inc_single_low  <- quantile(r2_inc_single_list, .025, na.rm=T)[[1]]
-    r2_inc_single_high <- quantile(r2_inc_single_list, .975, na.rm=T)[[1]]
-    r2_inc_single_mean <- mean(r2_inc_single_list)
+    r2_diff_single_SBayesR_single_LDpred_low  <- quantile(r2_diff_single_SBayesR_single_LDpred_list, .025, na.rm=T)[[1]]
+    r2_diff_single_SBayesR_single_LDpred_high  <- quantile(r2_diff_single_SBayesR_single_LDpred_list, .975, na.rm=T)[[1]]
+    r2_diff_single_SBayesR_single_LDpred_mean <- mean(r2_diff_single_SBayesR_single_LDpred_list)
+    
+    #r2_diff_single_LDpred_public_LDpred_low  <- quantile(r2_diff_single_LDpred_public_LDpred_list, .025, na.rm=T)[[1]]
+    #r2_diff_single_LDpred_public_LDpred_high  <- quantile(r2_diff_single_LDpred_public_LDpred_list, .975, na.rm=T)[[1]]
+    #r2_diff_single_LDpred_public_LDpred_mean <- mean(r2_diff_single_LDpred_public_LDpred_list)
 
-    r2_inc_multi_low  <- quantile(r2_inc_multi_list, .025, na.rm=T)[[1]]
-    r2_inc_multi_high <- quantile(r2_inc_multi_list, .975, na.rm=T)[[1]]
-    r2_inc_multi_mean <- mean(r2_inc_multi_list)
+    #r2_diff_multi_LDpred_public_LDpred_low  <- quantile(r2_diff_multi_LDpred_public_LDpred_list, .025, na.rm=T)[[1]]
+    #r2_diff_multi_LDpred_public_LDpred_high  <- quantile(r2_diff_multi_LDpred_public_LDpred_list, .975, na.rm=T)[[1]]
+    #r2_diff_multi_LDpred_public_LDpred_mean <- mean(r2_diff_multi_LDpred_public_LDpred_list)
 
-    r2_diff_single_public_low  <- quantile(r2_diff_single_public_list, .025, na.rm=T)[[1]]
-    r2_diff_single_public_high  <- quantile(r2_diff_single_public_list, .975, na.rm=T)[[1]]
-    r2_diff_single_public_mean <- mean(r2_diff_single_public_list)
+    #r2_diff_multi_LDpred_single_LDpred_low  <- quantile(r2_diff_multi_LDpred_single_LDpred_list, .025, na.rm=T)[[1]]
+    #r2_diff_multi_LDpred_single_LDpred_high  <- quantile(r2_diff_multi_LDpred_single_LDpred_list, .975, na.rm=T)[[1]]
+    #r2_diff_multi_LDpred_single_LDpred_mean <- mean(r2_diff_multi_LDpred_single_LDpred_list)
 
-    r2_diff_multi_public_low  <- quantile(r2_diff_multi_public_list, .025, na.rm=T)[[1]]
-    r2_diff_multi_public_high  <- quantile(r2_diff_multi_public_list, .975, na.rm=T)[[1]]
-    r2_diff_multi_public_mean <- mean(r2_diff_multi_public_list)
-
-    r2_diff_multi_single_low  <- quantile(r2_diff_multi_single_list, .025, na.rm=T)[[1]]
-    r2_diff_multi_single_high  <- quantile(r2_diff_multi_single_list, .975, na.rm=T)[[1]]
-    r2_diff_multi_single_mean <- mean(r2_diff_multi_single_list)
-
-
+    
     # print r2 confidence interval
-    print(paste0("Incremental R^2 public (95% CI): ", r2_inc_public_mean, " [", r2_inc_public_low, " : ", r2_inc_public_high, "]"))
-    print(paste0("Incremental R^2 single (95% CI): ", r2_inc_single_mean, " [", r2_inc_single_low, " : ", r2_inc_single_high, "]"))
-    print(paste0("Incremental R^2 multi (95% CI): ", r2_inc_multi_mean, " [", r2_inc_multi_low, " : ", r2_inc_multi_high, "]"))
-    print(paste0("Diff R^2 single - public (95% CI): ", r2_diff_single_public_mean, " [", r2_diff_single_public_low, " : ", r2_diff_single_public_high, "]"))
-    print(paste0("Diff R^2 multi - public (95% CI): ", r2_diff_multi_public_mean, " [", r2_diff_multi_public_low, " : ", r2_diff_multi_public_high, "]"))
-    print(paste0("Diff R^2 multi - single (95% CI): ", r2_diff_multi_single_mean, " [", r2_diff_multi_single_low, " : ", r2_diff_multi_single_high, "]"))
+    print(paste0("Incremental R^2 single LDpred(95% CI): ", r2_inc_single_LDpred_mean, " [", r2_inc_single_LDpred_low, " : ", r2_inc_single_LDpred_high, "]"))
+    print(paste0("Incremental R^2 single SBayesR(95% CI): ", r2_inc_single_SBayesR_mean, " [", r2_inc_single_SBayesR_low, " : ", r2_inc_single_SBayesR_high, "]"))
+    print(paste0("Diff R^2 single_SBayesR - single LDpred(95% CI): ", r2_diff_single_SBayesR_single_LDpred_mean, " [", r2_diff_single_SBayesR_single_LDpred_low, " : ", r2_diff_single_SBayesR_single_LDpred_high, "]"))
+    
+    #print(paste0("Incremental R^2 public LDpred(95% CI): ", r2_inc_public_LDpred_mean, " [", r2_inc_public_LDpred_low, " : ", r2_inc_public_LDpred_high, "]"))
+    #print(paste0("Incremental R^2 multi LDpred(95% CI): ", r2_inc_multi_LDpred_mean, " [", r2_inc_multi_LDpred_low, " : ", r2_inc_multi_LDpred_high, "]"))
+    #print(paste0("Diff R^2 single LDpred - public LDpred(95% CI): ", r2_single_LDpred_public_LDpred_mean, " [", r2_single_LDpred_public_LDpred_low, " : ", r2_diff_single_public_high, "]"))
+    #print(paste0("Diff R^2 multi LDpred - public LDpred(95% CI): ", r2_diff_multi_LDpred_public_LDpred_mean, " [", r2_diff_multi_LDpred_public_LDpred_low, " : ", r2_diff_multi_LDpred_public_LDpred_high, "]"))
+    #print(paste0("Diff R^2 multi LDpred - single LDpred(95% CI): ", r2_diff_multi_LDpred_single_LDpred_mean, " [", r2_diff_multi_LDpred_single_LDpred_low, " : ", r2_diff_multi_LDpred_single_LDpred_high, "]"))
+    
 
-    local_df$r2_inc_public_lower <- r2_inc_public_low
-    local_df$r2_inc_public_upper <- r2_inc_public_high
+    for (k in score_types){
+        assign(eval(paste0("local_df$r2_inc_",k,"_lower")), eval(parse(text=paste0("r2_inc_",k,"_low"))))
+        assign(eval(paste0("local_df$r2_inc_",k,"_upper")), eval(parse(text=paste0("r2_inc_",k,"_high"))))
+    }
+    #local_df$r2_diff_single_LDpred_public_LDpred_lower <- r2_diff_single_LDpred_public_LDpred_low
+    #local_df$r2_diff_single_LDpred_public_LDpred_upper <- r2_diff_single_LDpred_public_LDpred_high
 
-    local_df$r2_inc_single_lower <- r2_inc_single_low
-    local_df$r2_inc_single_upper <- r2_inc_single_high
+    #local_df$r2_diff_multi_LDpred_public_LDpred_lower <- r2_diff_multi_LDpred_public_LDpred_low
+    #local_df$r2_diff_multi_LDpred_public_LDpred_upper <- r2_diff_multi_LDpred_public_LDpred_high
 
-    local_df$r2_inc_multi_lower <- r2_inc_multi_low
-    local_df$r2_inc_multi_upper <- r2_inc_multi_high
+    #local_df$r2_diff_multi_LDpred_single_LDpred_lower <- r2_diff_multi_LDpred_single_LDpred_low
+    #local_df$r2_diff_multi_LDpred_single_LDpred_upper <- r2_diff_multi_LDpred_single_LDpred_high
 
-    local_df$r2_diff_single_public_lower <- r2_diff_single_public_low
-    local_df$r2_diff_single_public_upper <- r2_diff_single_public_high
-
-    local_df$r2_diff_multi_public_lower <- r2_diff_multi_public_low
-    local_df$r2_diff_multi_public_upper <- r2_diff_multi_public_high
-
-    local_df$r2_diff_multi_single_lower <- r2_diff_multi_single_low
-    local_df$r2_diff_multi_single_upper <- r2_diff_multi_single_high
-
+    local_df$r2_diff_single_SBayesR_single_LDpred_lower <- r2_diff_single_SBayesR_single_LDpred_low
+    local_df$r2_diff_single_SBayesR_single_LDpred_upper <- r2_diff_single_SBayesR_single_LDpred_high
     
     # print and record merged prediction sample N
     local_df$N <- nrow(data)
@@ -350,11 +327,11 @@ for (i in 1:length(pheno_names)){
 # low-level bootstraps
 fwrite(
   bootstrap_df,
-  paste0("derived_data/10_Prediction/output/bootstraps/",cohort,"_",method,"_bootstraps.txt")
+  paste0("derived_data/10_Prediction/output/bootstraps/",cohort,"_bootstraps.txt")
 )
 
 #results
 fwrite(
   df,
-  paste0("derived_data/10_Prediction/output/",cohort,"_",method,"_r2.txt")
+  paste0("derived_data/10_Prediction/output/",cohort,"_r2.txt")
 )
--- 10_Prediction/10.2_predict_phenotypes.sh
+++ 10_Prediction/10.2_predict_phenotypes.sh
@@ -6,9 +6,7 @@ cd $PGI_Repo
 Rscript code/10_Prediction/10.2.0_get_descriptives.R > $PGI_Repo/code/10_Prediction/10.2.0_get_descriptives.R.log
 
 # Run prediction
-for method in LDpred SBayesR
+for cohort in WLS UKB3 HRS2
 do
-    Rscript code/10_Prediction/10.2.1_predict_phenotypes.R HRS2 $method
-    Rscript code/10_Prediction/10.2.1_predict_phenotypes.R WLS $method
-    Rscript code/10_Prediction/10.2.1_predict_phenotypes.R UKB3 $method
+    Rscript code/10_Prediction/10.2.1_predict_phenotypes.R $cohort
 done
